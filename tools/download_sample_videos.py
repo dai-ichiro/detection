3e458804a4cb1d92c7cff1cb8a23ebd8af25ef4b
dai-ichiro <d.a.i-ichiro@outlook.jp>
2022-09-18 23:53:23 +0900

    change save dir

--- tools/download_sample_videos.py
+++ tools/download_sample_videos.py
@@ -1,8 +1,11 @@
+import os
 from torchvision.datasets.utils import download_url
 
+os.makedirs('videos', exist_ok=True)
+
 video1_url = 'https://github.com/dai-ichiro/robo-one/raw/main/video_1.mp4'
-download_url(video1_url, root = '.', filename = 'target.mp4')
+download_url(video1_url, root = 'videos', filename = 'target.mp4')
 
 video2_url = 'https://github.com/dai-ichiro/robo-one/raw/main/video_2.mp4'
 video2_fname = 'non_target.mp4'
-download_url(video2_url, root = '.', filename = 'non_target.mp4')
+download_url(video2_url, root = 'videos', filename = 'non_target.mp4')
